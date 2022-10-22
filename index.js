939ba7c4d8dee5517734e9fddea45b4fa3d1b00e
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-22 15:59:13 -0700

    clean up email validations

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const employeeArray = [];
 
 //Begin prompting user
 const addManager = () => {
-    
+
     console.log(`
     ===================
     Let's build a team!
@@ -28,7 +28,7 @@ const addManager = () => {
             name: `name`,
             message: `Please enter name of Team Manager:`,
             validate: userInput => {
-                if (userInput){
+                if (userInput) {
                     return true;
                 } else {
                     console.log(`Please enter name of Team Manager\n `)
@@ -41,7 +41,7 @@ const addManager = () => {
             name: `id`,
             message: `Please enter Team Manager's id #:`,
             validate: userInput => {
-                if (userInput){
+                if (userInput) {
                     return true;
                 } else {
                     console.log(`Please enter the id # of Team Manager\n `)
@@ -76,16 +76,16 @@ const addManager = () => {
                 }
             }
         },
-       
-    ])
-//Add manager's information to data array
-.then(managerData => {
-    const {name, id, email, officeNumber} = managerData;
-    const manager = new Manager (name, id, email, officeNumber);
 
-    employeeArray.push(manager);
-    console.log(manager);
-})
+    ])
+        //Add manager's information to data array
+        .then(managerData => {
+            const { name, id, email, officeNumber } = managerData;
+            const manager = new Manager(name, id, email, officeNumber);
+
+            employeeArray.push(manager);
+            console.log(manager);
+        })
 };
 
 //Add additional employees
@@ -100,41 +100,13 @@ const writeFile = data => {
 
 //Create team array and pass data into generateHTML
 addManager()
-.then(addEmployee)
-.then(employeeArray => {
-    return generateHTML(employeeArray);
-})
-.then(pageHTML => {
-    return writeFile(pageHTML);
-})
-.catch(err => {
-    console.log(err);
-});
-
-// Prompt/validate email
-// var inquirer = require('inquirer');
-// function promptUser() {
-//     return inquirer
-//       .prompt(
-//               {
-//           message: "Please enter your email",
-//           name: "email",
-//           type: "input",
-//           default: () => {},
-//           validate: function (email) {
-  
-//               valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-  
-//               if (valid) {
-//                 console.log("Great job");
-//                   return true;
-//               } else {
-//                   console.log(".  Please enter a valid email")
-//                   return false;
-//               }
-//           }
-//         }
-//       );
-//   }
-
-//   promptUser();
+    .then(addEmployee)
+    .then(employeeArray => {
+        return generateHTML(employeeArray);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
