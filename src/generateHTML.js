64b15106de798dd894ee49f466af3eabd64128ad
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-22 16:21:49 -0700

    pseudo code generateHTML

---
+++ src/generateHTML.js
@@ -0,0 +1,25 @@
+//Divs for employee cards:
+//Manager
+const generateManager = function (manager) {
+    return ` `;
+};
+//Engineer
+const generateEngineer = function (engineer) {
+    return ` `;
+};
+//Intern
+const generateIntern = function (intern) {
+    return ` `;
+};
+
+
+
+//Push employee html cards into array.
+generateHTML = (data) => {
+
+};
+
+//Generate page HTML using employee card array
+
+//Export to index
+module.exports = generateHTML;
