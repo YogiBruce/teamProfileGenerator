219fae7ea03827a047323549af66b558448d4b46
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-22 10:49:46 -0700

    intern test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,7 +1,23 @@
-//Intern test
+//Intern test using intern constructor
+const Intern = require(`../lib/Intern`);
 
-//School
+test(`creates an intern object`, () => {
+    const intern = new Intern(`Robert`, 85, 'da.bruce.jr@gmail.com', 'NAU');
 
-//getSchool()
+    expect(intern.school).toEqual(expect.any(String));
+});
 
-//getRole()
+//Test getSchool()
+test(`gets employee school`, () => {
+    const intern = new Intern(`Robert`, 85, 'da.bruce.jr@gmail.com', 'NAU');
+
+    expect(intern.school()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+//Test getRole()
+test(`gets role of employee`, () => {
+    const intern = new Intern(`Robert`, 85, 'da.bruce.jr@gmail.com', 'NAU');
+
+    expect(intern.getRole()).toEqual("Intern");
+
+});
