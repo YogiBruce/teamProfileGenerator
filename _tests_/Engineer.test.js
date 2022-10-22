523c7eafc67c644f24656be1b34b700ed43f7240
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-22 10:33:14 -0700

    engineer test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,7 +1,22 @@
-//Engineer test
+//Engineer test using Engineer constructor 
+const Engineer = require(`../lib/Engineer`);
 
-//gitHub username
+test(`creates an Engineer object`, () => {
+    const engineer = new Engineer(`Robert`, 85, `da.bruce.jr@gmail.com`, `YogiBruce`);
 
-//getGitHub
+    expect(engineer.github).toEqual(expect.any(String));
+});
 
-//getRole
+//Tests gitHub username from getGithub()
+test(`gets engineer github username`, () => {
+    const engineer = new Engineer(`Robert`, 85, `da.bruce.jr@gmail.com`, `YogiBruce`);
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+//Tests role from getRole()
+test(`gets role of employee`, () => {
+    const engineer = new Engineer(`Robert`, 85, `da.bruce.jr@gmail.com`, `YogiBruce`);
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
