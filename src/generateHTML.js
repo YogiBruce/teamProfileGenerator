402c0ad92bee41e3d9fb514ebcdbec0a905ce12f
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-10-23 09:40:45 -0700

    fix bootstrap icons and layout

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,11 +1,44 @@
 //Divs for employee cards:
 //Manager
 const generateManager = function (manager) {
-    return ` `;
+    return `<div class="col-4 mt-4">
+    <div class="card h-100">
+        <div class="card-header">
+            <h3>${manager.name}</h3>
+            <div class="row"><i class="fa-solid fa-mug-hot pr-2"></i>
+                <h4>Manager</h4>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <p class="id">ID: ${manager.id}</p>
+            <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+            <p class="office">Office Number: ${manager.officeNumber}</p>
+        </div>
+    </div>
+</div> `;
 };
+
 //Engineer
 const generateEngineer = function (engineer) {
-    return ` `;
+    return `<div class="col-4 mt-4">
+    <div class="card h-100">
+        <div class="card-header">
+            <h3>${engineer.name}</h3>
+            <div class="row"><i class="fa-solid fa-glasses pr-2"></i>
+                <h4>Engineer</h4>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <p class="id">ID: 1985</p>
+            <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
+            </p>
+            <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+            </p>
+        </div>
+    </div>
+</div> `;
 };
 //Intern
 const generateIntern = function (intern) {
